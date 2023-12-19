481fd9ead24d002b0005c249977f466c54e982d6
Ruksaar786 <adamruksaar1@gmail.com>
2023-12-19 10:50:36 +0200

    weather.js file added
    added weather.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello </h1>
+        <h1>hello from react</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
